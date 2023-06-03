00a6f71802c80c8c994b818f30d7c0e99d142d50
Mohammed-Ayman710 <mohammedayna7@gmail.com>
2023-06-03 03:57:59 +0300

    update

---
+++ app.js
@@ -0,0 +1,40 @@
+/*
+let firstName="Mohammed";
+let lastName="Ayman";
+let major="cis";
+console.log(firstName.toUpperCase(),"@",lastName.toUpperCase(),"@",major.toUpperCase());
+*/
+
+let mark=prompt("Please Entre The mark");
+
+
+if(mark >=0 && mark<=49){
+    console.log("sorry you hava faild");
+}
+else if(mark >=50 && mark <=60){
+    console.log("he will D")
+}
+else if(mark >=61 && mark <=70){
+    console.log("he will C")
+}
+else if(mark >=71 && mark <=80){
+    console.log("he will B")
+}
+else if(mark >=81 && mark <=90){
+    console.log("he will A")
+}
+else if(mark >=91 && mark <=100){
+    console.log("he will A+")
+}
+else{
+    console.log("values incoorect");
+}
+
+
+
+
+
+
+
+
+
